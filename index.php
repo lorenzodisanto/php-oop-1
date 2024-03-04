42553eebfe6b05a98f7478a855dc59e3a127b3e7
lorenzodisanto <lore.disanto95@gmail.com>
2024-03-04 16:29:06 +0100

    feat: print product

--- index.php
+++ index.php
@@ -4,4 +4,24 @@ class Production {
     public $title;
     public $language;
     public $vote;
+
+    // costruttore
+    function __construct(
+        string $title,
+        string $language,
+        int $vote,
+    ){
+        $this->title = $title;
+        $this->language = $language;
+        $this->vote = $vote;
+    }
+
 }
+
+
+// creazione istanza
+$first_product = new Production("Batman","Inglese", 9);
+var_dump($first_product);
+
+$second_product = new Production("Superman","Inglese", 8);
+var_dump($second_product);
