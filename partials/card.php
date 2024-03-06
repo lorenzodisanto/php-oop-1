f10b27e5f3cb103686e817727d8da0734ecc2efb
lorenzodisanto <lore.disanto95@gmail.com>
2024-03-06 11:56:05 +0100

    fix: card code

--- partials/card.php
+++ partials/card.php
@@ -2,41 +2,45 @@
   
   <!-- Titolo -->
   <h3 class="card-header"><?= $product->get_title() ?></h3>
-  <div class="card-body">
-    <!-- descrizione -->
+
+  <!-- descrizione -->
+  <div class="card-body">    
     <p class="card-text">
-    <?php foreach( $product->genres as $genre ) : ?>
-        <?= $genre->description ?></p>
-        <?php endforeach; ?>
+      <?php foreach( $product->genres as $genre ) : ?>
+          <?= $genre->description ?>
+      <?php endforeach; ?>
     </p>
   </div>
+
+  <!-- Lista -->
   <ul class="list-group list-group-flush">
-       <!-- lingua e voto -->
+    
+    <!-- lingua e voto -->
     <li class="list-group-item"><b>Lingua:</b> <?= $product->language ?></li>
     <li class="list-group-item"><b>Voto:</b> <?= $product->vote ?></li>
 
-     <!-- generi -->
+    <!-- generi -->
     <li class="list-group-item">
       <b>Genere:</b>
       <?php foreach( $product->genres as $genre ): ?>
-      <?= $genre->name ?>
+       <?= $genre->name ?>
       <?php endforeach; ?>
     </li>
 
-    <!-- se è un Movie -->
+    <!-- se è un Movie stampo incassi e durata-->
     <?php if($product instanceof Movie): ?>
-    <li class="list-group-item">
-    <p><b>Incassi:</b> <?= $product->profits ?> €</p>
-    </li>
-    <li class="list-group-item">
-    <p><b>Durata:</b> <?= $product->duration ?> min</p>
-    </li>
+     <li class="list-group-item">
+       <p><b>Incassi:</b> <?= $product->profits ?> €</p>
+     </li>
+     <li class="list-group-item">
+       <p><b>Durata:</b> <?= $product->duration ?> min</p>
+     </li>
 
-    <!-- se è una TVSerie -->
-    <?php elseif($product instanceof TVSerie): ?>
-    <li class="list-group-item">
-    <p><b>Numero stagioni: </b><?= $product->seasons ?></p>
-    </li>
-    <?php endif ?>   
+     <!-- se è una TVSerie stampo stagioni-->
+     <?php elseif($product instanceof TVSerie): ?>
+      <li class="list-group-item">
+       <p><b>Numero stagioni: </b><?= $product->seasons ?></p>
+      </li>
+    <?php endif; ?>   
   </ul>
 </div> 
